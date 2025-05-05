cf65760f30f909af2e907936bb757b74ad07e9ca
Boris Arturo González Rivera <Bgonzalez@intergrupo.com>
2025-05-05 16:14:16 -0500

    Add project files.

---
+++ PoCBus/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+
+var builder = new ConfigurationBuilder()
+ .AddUserSecrets<Program>();
+
+var configuration = builder.Build();
+
+string sbConnectionString = configuration["ServiceBusConnectionString"];
+Console.WriteLine($"Sb ConnectionString: {sbConnectionString}");
+
+string queueName1 = configuration["queueName1"];
+Console.WriteLine($"queueName1: {queueName1}");
+
+if(sbConnectionString is  null || queueName1 is null)
+{
+    Console.WriteLine("Error de configuración!");
+    Console.ReadKey();
+}
+
+Console.WriteLine("Inicio exportacion de mensajes de la cola!");
+await ServiceBusExportMessage(sbConnectionString, queueName1);
+Console.WriteLine("Terminacion exportacion de mensajes de la cola!");
+
+Console.ReadKey();
+
+
+static async Task ServiceBusExportMessage(string serviceBusConnectionString, string queueName)
+{
+    await using var client = new ServiceBusClient(serviceBusConnectionString);
+    ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+    string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmm");
+    string fileName = $"mensajes_pendientes-{queueName}-{timestamp}.txt";
+    using StreamWriter file = new StreamWriter(fileName, append: false, encoding: Encoding.UTF8);
+    int i=0;
+    while (true)
+    {
+        var messages = await receiver.PeekMessagesAsync(maxMessages: 5000);//Recibir sin eliminarlos
+        i++;
+        Console.WriteLine($"Recuperó {i*5000}");
+
+        if (messages.Count == 0)
+        {
+            break;
+        }
+
+        foreach (var message in messages)
+        {
+            // Escribir el contenido del mensaje en el archivo
+            await file.WriteLineAsync(message.Body.ToString());
+        }
+        Console.WriteLine($"Almacenó {i * 5000}");
+    }
+
+    Console.WriteLine($"Mensajes guardados en '{fileName}'");
+
+}
+
+
+
+
---
+++ RecoveryTransaction/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Nethereum.Web3;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<Program>();
+
+        var configuration = builder.Build();
+        string nodeUrl = configuration["nodeUrl"];
+
+        // Dirección del nodo Ethereum (puede ser Infura, Alchemy, etc.)
+        
+        var web3 = new Web3(nodeUrl);
+
+        // Hash de la transacción que quieres consultar
+        var txHash = "0x2a3dfc715a1b6f107d3d1d548b0d03682440ff8435d17b0ca1fcc34aa7e98cce";
+
+        // Obtener el recibo de la transacción
+        var receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(txHash);
+
+        if (receipt != null)
+        {
+            Console.WriteLine($"Transaction Hash: {receipt.TransactionHash}");
+            Console.WriteLine($"Block Number (hex): {receipt.BlockNumber.Value.ToString("X")}");
+            Console.WriteLine($"Block Number (decimal): {receipt.BlockNumber.Value}");
+            Console.WriteLine($"Status: {receipt.Status.Value}");
+        }
+        else
+        {
+            Console.WriteLine("Transaction receipt not found (may not be mined yet).");
+        }
+    }
+}
